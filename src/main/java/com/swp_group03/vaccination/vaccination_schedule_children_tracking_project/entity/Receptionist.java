edee70e16ff68151762937fd23aa51796e32bb91
YukiiMin <hoangtuanminh@gmail.com>
2025-02-17 13:56:56 +0700

    Config connect DB

--- src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/entity/Receptionist.java
+++ src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/entity/Receptionist.java
@@ -1,4 +1,18 @@
 package com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Receptionist extends Authenticated_User{
+    private String receptionist_id;
+    private String receptionist_password;
+    private String receptionist_name;
+    private String receptionist_email;
+
 }
